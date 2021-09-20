a879b82f4c17d4d79ed72d05c0de01b01a0b7a4d
Juan Manuel Pedrozo <juanmapedrozo@gmail.com>
2021-09-20 18:10:26 -0300

    carrito

--- Files/app.js
+++ Files/app.js
@@ -5,10 +5,12 @@ const path = require('path');
 app.listen(3031, ()=>
 console.log("Server start at port 3031"));
 
-app.get('/', (req, res)=>{
+/*app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "Views/home.html"))
+});*/
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "Views/carrito1.html"))
 });
-
 app.use(express.static(path.resolve(__dirname,'./Public')));
 
 
