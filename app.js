a68992c7f1b0e8449a1eecc53f181eed3b60f1b2
Ifabiani <ignaciofabiani@gmail.com>
2021-10-01 18:07:34 -0300

    Ordenando las carpetas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.listen(3031, ()=>
-console.log("Server start at port 3031"));
+console.log("http://localhost:3031"));
 
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "Views/home.html"))
