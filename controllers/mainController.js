591228fb2bb6e97bdb9091cff4fcf3e98f8db69f
Ifabiani <ignaciofabiani@gmail.com>
2021-10-24 01:54:00 -0300

    cambios html

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+const productsFilePath = path.join(__dirname, '../Data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'))
 
 const mainController = {
     home: (req, res) => {
-        res.render("home")
+        res.render("home", {products:products})
     },
 
     
